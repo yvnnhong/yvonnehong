3a02ffc97b8792c97ff942b79752626e807ca5b1
Yvonne Hong <yvonnehong2003@gmail.com>
2025-08-01 11:40:21 -0700

    fixed trailing slash issue??!

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/yvonnehong/',  // Make sure this matches your GitHub repository name
+  base: '/yvonnehong',  // HAS TO MATCH GITHUB REPO NAME + REMOVE TRAILING SLASH!
 })
