b1e3e9e9ddf95d8ff999ccbf8832ddd319f990c9
Yvonne Hong <yvonnehong2003@gmail.com>
2025-08-01 12:23:17 -0700

    do i need a trailing slash or no in viteconfig.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+//either i need a trailing slash or i dont???!! idk!!
 
 // https://vitejs.dev/config/
 export default defineConfig({
